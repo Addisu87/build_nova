98df1df00b97a659016f14896a9dfd069e1241e2
Addisu <addisuhaile87@gmail.com>
2025-03-11 15:10:12 +0300

    Update files

--- lib/supabase/db.ts
+++ lib/supabase/db.ts
@@ -1,15 +1,6 @@
 import { createClient } from "@supabase/supabase-js"
 import { Database } from "@/types/supabase"
-
-const supabaseUrl =
-	process.env.NEXT_PUBLIC_SUPABASE_URL!
-const supabaseAnonKey =
-	process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-
-export const supabase = createClient<Database>(
-	supabaseUrl,
-	supabaseAnonKey,
-)
+import { supabase } from "./client"
 
 export class DatabaseError extends Error {
 	constructor(
